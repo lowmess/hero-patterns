100d188225d9cefb1bfc79015f3e15e654af1e16
Alec Lomas <alec@lowmess.com>
2016-11-29 02:23:11 -0700

    Revert "fix: incorrectly formatted exports"
    This reverts commit 25849299b90cfe3db35c11008e4197c7dfbe6931.

--- patterns/rails.js
+++ patterns/rails.js
@@ -1,3 +1,5 @@
 import { defaultFill, defaultOpacity } from './_defaults.js'
 
-export const rails = (fill = defaultFill, opacity = defaultOpacity) => `url('data:image/svg+xml,%3Csvg%20width%3D%2220%22%20height%3D%2210%22%20viewBox%3D%220%200%2020%2010%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%3Ctitle%3Erails%3C%2Ftitle%3E%3Cpath%20d%3D%22M16%206H6v4H4V6H2V4h2V0h2v4h10V0h2v4h2v2h-2v4h-2V6z%22%20fill%3D%22${fill}%22%20fill-opacity%3D%22${opacity}%22%20fill-rule%3D%22evenodd%22%2F%3E%3C%2Fsvg%3E')`
+const rails = (fill = defaultFill, opacity = defaultOpacity) => `url('data:image/svg+xml,%3Csvg%20width%3D%2220%22%20height%3D%2210%22%20viewBox%3D%220%200%2020%2010%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%3Ctitle%3Erails%3C%2Ftitle%3E%3Cpath%20d%3D%22M16%206H6v4H4V6H2V4h2V0h2v4h10V0h2v4h2v2h-2v4h-2V6z%22%20fill%3D%22${fill}%22%20fill-opacity%3D%22${opacity}%22%20fill-rule%3D%22evenodd%22%2F%3E%3C%2Fsvg%3E')`
+
+export default rails
